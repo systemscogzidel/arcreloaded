9c46dd100b031cc84153db3c7594183a5ee666a7
Vishnu Priya <systems@cogzidel.com>
2018-02-10 09:10:10 +0530

    new source added-vishnu priya

--- sitesetting/templates.py/url.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from new_app import views
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),   
-    url(r'^$', views.sitesetting, name='index'),
-
-]
