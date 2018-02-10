09d1404e544b8be4eb041feaaaffe22b6b6be6ee
Vishnu Priya <systems@cogzidel.com>
2018-02-10 10:12:03 +0530

    updated in settings.py-vishnu priya

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,9 +25,9 @@ STATIC_DIR = os.path.join(BASE_DIR,"static")
 SECRET_KEY = 't46cfs_-jvm2%=s&d!4t6^o29msuk8_$zfeo52wi1qu=x=bib3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [‘0.0.0.0:5000’, ‘localhost’, ‘arcreloaded.herokuapp.com’]
 
 
 # Application definition
